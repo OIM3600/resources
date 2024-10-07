0ca37de9a125461d7b7b58754ac4814ac3c2f26d
Zhi Li <lzblack@users.noreply.github.com>
2024-10-06 20:05:49 -0400

    Update weather.py

--- code/weather.py
+++ code/weather.py
@@ -1,12 +1,18 @@
-import urllib.request
 import json
+import os
+import urllib.request
+
+from dotenv import load_dotenv
+
+load_dotenv()
+APIKEY = os.getenv("API_KEY")
 
 
 def get_weather_data(city_name, country_code, api_key):
     """
     Get weather data for a specified city and country using the OpenWeatherMap API.
     """
-    api_url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name},{country_code}&APPID={api_key}'
+    api_url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name},{country_code}&APPID={api_key}'
 
     with urllib.request.urlopen(api_url) as response:
         response_text = response.read().decode('utf-8')
@@ -23,11 +29,10 @@ def get_temp(weather_data):
 
 
 def main():
-    API_KEY = 'YOUR_OWN_APIKEY'
     city_name = 'Wellesley'
     country_code = 'us'
 
-    weather_data = get_weather_data(city_name, country_code, API_KEY)
+    weather_data = get_weather_data(city_name, country_code, APIKEY)
     print(weather_data)
 
 
